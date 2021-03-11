ba9eaf78f05a60ad82272dd349d8ef1ea2349c45
Robert Lyons <nextdude@gmail.com>
2021-03-11 11:15:15 -0500

    fix id64.js reference to esm lib

--- id64.js
+++ id64.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { argv } from "node:process";
-import * as id64 from "./lib/esm/index.js";
+import id64 from "./lib/esm/index.mjs";
 const n = Number(argv[2] || '1');
 console.log([...Array(n).keys()].map(_ => id64.gen()).join('\n'));
